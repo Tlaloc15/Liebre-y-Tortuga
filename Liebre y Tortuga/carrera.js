542f9efcfc8df25691c14d345854ccc481ba2cb6
Carlos Eduardo Garcia Estrada <42812352+Tlaloc15@users.noreply.github.com>
2019-09-06 13:36:03 -0500

    Actualizado

--- Liebre y Tortuga/carrera.js
+++ Liebre y Tortuga/carrera.js
@@ -1,7 +1,93 @@
+class Corredor {
+    constructor(nombre, posicionLT) {
+        this._nombre = nombre;
+        this._posicionLT = posicionLT;
+    }
+
+    get nombre(){
+        return this
+    }
+
+    get posicion(){
+        return this._posicionLT;
+    }
+
+    set posicion(posicionLT = 0){
+        this._posicionLT = posicionLT;
+
+    }
+}
+
+
+
+class Dado{
+    constructor() {
+
+    }
+    lanzar(){
+        return Math.trunc(Math.random() * probabilidadLT + 1);
+        
+    }
+}
+
+
+class Liebre extends Corredor{
+    constructor(posicionLT, probabilidadLT){
+        super(posicionLT);
+        this._probabilidad = probabilidadLT;
+
+    }
+
+    avanceLiebre(pasoLiembre){
+        probabilidadLT = 20;
+        var pasoLiembre = d1.lanzar(probabilidadLT);
+        if (l == 1 || pasoLiembre == 2) { //10%
+            posicionLT += -12;
+        } else if (pasoLiembre == 3 || pasoLiembre == 4 || pasoLiembre == 5) { //15%
+            posicionLT += -2;
+        } else if (pasoLiembre == 6 || pasoLiembre == 7 || pasoLiembre == 8 || pasoLiembre == 9) { //20%
+            posicionLT += 0;
+        } else if (pasoLiembre == 10 || pasoLiembre == 11 || pasoLiembre == 12 || pasoLiembre == 13) { //20%
+            posicionLT += 9;
+        } else {  //35%
+            posicionLT += 1;
+    }
+        }
+}
+
+class Tortuga extends Corredor{
+    constructor(){
+        super(posicionLT)
+
+    }
+    probabilidadLT = 10;
+
+    avanceTortuga(pasoTortuga){
+        this.probabilidadLT = 10;
+        var pasoTortuga = d1.lanzar(this.probabilidadLT);
+        if (pasoTortuga == 1 || pasoTortuga == 2) { //20%
+            posicionLT += -6;
+        } else if (pasoTortuga == 3 || pasoTortuga == 4 || pasoTortuga == 5) { //30%
+            posicionLT += 1;
+        } else {  //50%
+            posicionLT += 3;
+        }
+        }
+}
+
 class Carrera{
     constructor(){
     }
     ganador(){
+        var liebre = new Liebre();
+        var tortuga = new Tortuga();
+        
+        while(tortuga.posicionLT < 90 && liebre.posicionLT < 90){
+            tortuga.avanceTortuga();
+            liebre.avanceLiebre();
+            console.log('posicion Tortuga ' + tortuga.posicionLT);
+            console.log('posicion Liebre ' + liebre.posicionLT);
+        }
         if(pasoLiembre > 90 && pasoTortuga < 90) {
             console.log('Liebre gana la carrera');
         }
@@ -13,73 +99,8 @@ class Carrera{
         }
         }
 
-
-}
-
-var carrera = new Carrera;
-pasoLiembre = 0; 0
-pasoTortuga = 0;
-while (i <= 90) {
-
-}
-
-class Dado{
-    constructor() {
-
-    }
-    lanzar(){
-        return Math.trunc(Math.random() * probabilidadLT + 1);
-    }
-}
-
-
-class Liebre{
-    constructor(probabilidadLT = 0){
-        this._posicion = pos[0];
-        this._probabilidad = probabilidadLT;
-
-    }
-    
-    get posicion(){
-        return this._posicion;
-    }
-    get probabilidad(){
-        return this._probabilidad;
     }
 
-    probabilidadLT = 20;
-    d1 = new Dado;
-
-    avance(pasoLiembre){
-        var pasoLiembre = d1.lanzar(probabilidadLT);
-        if (pasoLiembre == 1 || pasoLiembre == 2) { //10%
-            pos[0] += -12;
-        } else if (pasoLiembre == 3 || pasoLiembre == 4 || pasoLiembre == 5) { //15%
-            pos[0] += -2;
-        } else if (pasoLiembre == 6 || pasoLiembre == 7 || pasoLiembre == 8 || pasoLiembre == 9) { //20%
-            pos[0] += 0;
-        } else if (pasoLiembre == 10 || pasoLiembre == 11 || pasoLiembre == 12 || pasoLiembre == 13) { //20%
-            pasoLiembre += 9;
-        } else {  //35%
-            pos[0] += 1;
-    }
-        }
-}
-
-class Tortuga extends Liebre{
-    constructor(){
-        super(this._posicion, this._probabilidadLT)
-
-    }
-    probabilidadLT = 10;
-
-    avance(pasoTortuga){
-        if (pasoTortuga == 1 || pasoTortuga == 2) { //20%
-            pasoTortuga += -6;
-        } else if (pasoTortuga == 3 || pasoTortuga == 4 || pasoTortuga == 5) { //30%
-            pasoTortuga += 1;
-        } else {  //50%
-            pasoTortuga += 3;
-        }
-        }
-}
+d1 = new Dado;
+var ganar = new Carrera();
+console.log(ganar.ganador());
